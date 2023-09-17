263fc6a1167dd4f800eaf5a9d7ffb6a89ff9501d
Avez-Anwar <avezanwar6@gmail.com>
2023-09-17 15:59:02 +0530

    Initial UI changes add for SPS game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import NoteIcon from "@mui/icons-material/Note";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import { useState } from "react";
 
 function App() {
+  const [score, serScore] = useState(0);
+  const handleStoneClicked = () => {
+    console.log("stone button clicked");
+  };
+  const handlePapperClicked = () => {
+    console.log("paper button clicked");
+  };
+  const handleSissorsClicked = () => {
+    console.log("sissors button clicked");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-section">
+        <div className="App">
+          <div className="game-elements">
+            <h1>Rock</h1>
+            <h1>Paper</h1>
+            <h1>Scissors</h1>
+          </div>
+          <div className="score">
+            <h1>
+              Score
+              <br />
+              {score}
+            </h1>
+          </div>
+        </div>
+        <div className="icons">
+          <button onClick={handleStoneClicked}>
+            <SportsSoccerIcon
+              sx={{ fontSize: 100, color: "grey", padding: "0 50px" }}
+            />
+          </button>
+          <button onClick={handlePapperClicked}>
+            <NoteIcon
+              sx={{ fontSize: 100, color: "grey", padding: "0 50px" }}
+            />
+          </button>
+          <button onClick={handleSissorsClicked}>
+            <RestaurantIcon
+              sx={{ fontSize: 100, color: "grey", padding: "0 50px" }}
+            />
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
