d172abdda9806fff6b6271769883e749c4e6f92d
Avez-Anwar <avezanwar6@gmail.com>
2023-09-18 21:41:52 +0530

    RPS code optimized

--- src/App.js
+++ src/App.js
@@ -9,27 +9,10 @@ import Paper from "./component/Paper";
 import Sisscors from "./component/Sisscors";
 
 function App() {
-  const [score, serScore] = useState(0);
+  const [score, setScore] = useState(0);
   const [showRock, setShowRock] = useState(false);
   const [randomIcon, setRandomIcon] = useState(null);
   const [sameButton, setSameButton] = useState(null);
-
-  const handleStoneClicked = () => {
-    setSameButton("Rock");
-    const randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-      case 0:
-        return "Rock";
-      case 1:
-        return "Paper";
-      case 2:
-        return "Scissors";
-      default:
-        return null;
-    }
-    // setShowRock(!showRock);
-    // console.log("stone button clicked");
-  };
 
   const handlePaperClicked = () => {
     setSameButton("Paper");
@@ -62,61 +45,46 @@ function App() {
   };
   let content;
   if (sameButton === "Rock" && randomIcon === "Rock") {
-    content = (
-      <div>
-        <h1>Draw</h1>
-      </div>
-    );
+    content = "Draw";
   } else if (sameButton === "Rock" && randomIcon === "Paper") {
-    content = (
-      <div>
-        <h1>YOU LOSE</h1>
-      </div>
-    );
+    content = "YOU LOSE";
   } else if (sameButton === "Rock" && randomIcon === "Scissors") {
-    content = (
-      <div>
-        <h1>YOU WIN</h1>
-      </div>
-    );
+    content = "YOU WIN";
   } else if (sameButton === "Paper" && randomIcon === "Paper") {
-    content = (
-      <div>
-        <h1>Draw</h1>
-      </div>
-    );
+    content = "Draw";
   } else if (sameButton === "Paper" && randomIcon === "Rock") {
-    content = (
-      <div>
-        <h1>YOU WIN</h1>
-      </div>
-    );
+    content = "YOU WIN";
   } else if (sameButton === "Paper" && randomIcon === "Scissors") {
-    content = (
-      <div>
-        <h1>YOU LOSE</h1>
-      </div>
-    );
+    content = "YOU LOSE";
   } else if (sameButton === "Scissors" && randomIcon === "Scissors") {
-    content = (
-      <div>
-        <h1>Draw</h1>
-      </div>
-    );
+    content = "Draw";
   } else if (sameButton === "Scissors" && randomIcon === "Paper") {
-    content = (
-      <div>
-        <h1>YOU WIN</h1>
-      </div>
-    );
+    content = "YOU WIN";
   } else if (sameButton === "Scissors" && randomIcon === "Rock") {
-    content = (
-      <div>
-        <h1>YOU LOSE</h1>
-      </div>
-    );
+    content = "YOU LOSE";
   }
   console.log(content);
+  const handleStoneClicked = () => {
+    setSameButton("Rock");
+    if (content === "Draw") {
+      setScore(score + 0);
+    } else if (content === "YOU WIN") {
+      setScore(score + 1);
+    } else if (content === "YOU LOSE") {
+      setScore(score - 1);
+    }
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return "Rock";
+      case 1:
+        return "Paper";
+      case 2:
+        return "Scissors";
+      default:
+        return null;
+    }
+  };
   return (
     <>
       <div className="main-section">
